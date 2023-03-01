54705517cb03e60f81f9aceaf86ca10dffeab7bf
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-01 20:33:22 -0300

    feat: add express route adapter

--- src/main/routes/signup.routes.test.ts
+++ src/main/routes/signup.routes.test.ts
@@ -1,23 +1,24 @@
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helper/MongoHelper'
 import app from '../config/app'
+import env from '../config/env'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    MongoHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(global.__MONGO_URI__)
-    })
-
-    beforeEach(async () => {
-      const accountCollection = await MongoHelper.getCollection('accounts')
-
-      await accountCollection.deleteMany({})
-    })
-
-    afterAll(async () => {
-      MongoHelper.disconnect()
-    })
-
     await request(app)
       .post('/api/signup')
       .send({
--- src/main/routes/signup.routes.ts
+++ src/main/routes/signup.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/expressRouteAdapter'
+import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router) => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
