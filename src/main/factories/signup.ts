9dcb546155d5a9868ef81e3275125b60d792de81
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-06 20:44:52 -0300

    feat: pass log mongo repository to constructor

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccountUseCase } from '../../data/usecases/addAccount/DbAddAccountUseCase'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/AccountMongoRepository'
+import { LogMongoRepository } from '../../infra/db/mongodb/LogMongoRepository'
 import { SignUpController } from '../../presentation/controllers/signup/SignUpController'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
@@ -13,6 +14,7 @@ export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
   const addAccount = new DbAddAccountUseCase(encrypter, addAccountRepository)
   const controller = new SignUpController(emailValidator, addAccount)
+  const logMongoRepository = new LogMongoRepository()
 
-  return new LogControllerDecorator(controller)
+  return new LogControllerDecorator(controller, logMongoRepository)
 }
