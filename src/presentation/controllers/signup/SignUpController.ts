443c47fb333b1b3c2586b480d1f56b3a8ae969f6
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-09 19:56:33 -0300

    refactor: remove required field validation

--- src/presentation/controllers/signup/SignUpController.ts
+++ src/presentation/controllers/signup/SignUpController.ts
@@ -19,14 +19,6 @@ export class SignUpController implements Controller {
         return badRequest(okOrError)
       }
 
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
-
       const { name, email, password, passwordConfirmation } = httpRequest.body
 
       if (password !== passwordConfirmation) {
