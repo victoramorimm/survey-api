64c3f2b24af6d1cdefefd7e7d9b039d182103cf2
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-09 21:05:31 -0300

    refactor: change error

--- src/presentation/helpers/validators/CompareFieldsValidation.ts
+++ src/presentation/helpers/validators/CompareFieldsValidation.ts
@@ -9,7 +9,7 @@ export class CompareFieldsValidation implements Validation {
 
   validate (input: any): Error {
     if (input[this.fieldName] !== input[this.fieldToCompare]) {
-      return new InvalidParamError(this.fieldName)
+      return new InvalidParamError(this.fieldToCompare)
     }
   }
 }
