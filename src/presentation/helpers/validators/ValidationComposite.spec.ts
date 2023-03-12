ee93ef7b43db6073578fe30bc67302ae70b16227
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-12 08:37:28 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/ValidationComposite.spec.ts
+++ src/presentation/helpers/validators/ValidationComposite.spec.ts
@@ -13,30 +13,52 @@ const makeValidation = (): Validation => {
 
 type SutTypes = {
   sut: ValidationComposite;
-  validation: Validation
+  validations: Validation[]
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidation()
+  const firstValidation = makeValidation()
+  const secondValidation = makeValidation()
 
-  const sut = new ValidationComposite([validationStub])
+  const sut = new ValidationComposite([firstValidation, secondValidation])
 
   return {
     sut,
-    validation: validationStub
+    validations: [firstValidation, secondValidation]
   }
 }
 
 describe('Validation Composite', () => {
   test('should return an error if any validation fails', () => {
-    const { sut, validation } = makeSut()
+    const { sut, validations } = makeSut()
 
     const mockedError = new Error('mocked_error')
 
-    jest.spyOn(validation, 'validate').mockReturnValueOnce(mockedError)
+    jest.spyOn(validations[0], 'validate').mockReturnValueOnce(mockedError)
 
     const validateResult = sut.validate({ field: 'any_value' })
 
     expect(validateResult).toEqual(mockedError)
+  })
+
+  test('should return the first error if more than one validation failed', () => {
+    const { sut, validations } = makeSut()
+
+    const mockedError = new Error('first_error')
+
+    jest.spyOn(validations[0], 'validate').mockReturnValueOnce(mockedError)
+    jest.spyOn(validations[1], 'validate').mockReturnValueOnce(new Error('second_error'))
+
+    const validateResult = sut.validate({ field: 'any_value' })
+
+    expect(validateResult).toEqual(mockedError)
+  })
+
+  test('should not return if all validations succeeds', () => {
+    const { sut } = makeSut()
+
+    const validateResult = sut.validate({ field: 'any_value' })
+
+    expect(validateResult).toBeFalsy()
   })
 })
