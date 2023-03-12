4001565b0ee1bbe824aab132929b9b39e98aa97d
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-12 08:14:55 -0300

    test: validation

---
+++ src/presentation/helpers/validators/CompareFieldsValidation.spec.ts
@@ -0,0 +1,30 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './CompareFieldsValidation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('Compare Fields Validation', () => {
+  test('should return an InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({
+      field: 'correct_value',
+      fieldToCompare: 'incorrect_value'
+    })
+
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    const sut = makeSut()
+
+    const result = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+
+    expect(result).toBeFalsy()
+  })
+})
