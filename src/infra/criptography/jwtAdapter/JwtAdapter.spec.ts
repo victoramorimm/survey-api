5480d2291dc0098cd861d2360069e16586e3cdbd
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-20 19:01:08 -0300

    tet: ensure JwtAdapter throws if sign method throws

--- src/infra/criptography/jwtAdapter/JwtAdapter.spec.ts
+++ src/infra/criptography/jwtAdapter/JwtAdapter.spec.ts
@@ -27,4 +27,16 @@ describe('Jwt Adapter', () => {
 
     expect(accessToken).toBe('any_token')
   })
+
+  test('should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
