633f44b714f15eda67450e23fa0d6b91f838599b
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-20 19:50:50 -0300

    feat: ensure AccountMongo returns an account on loadByEmail success

--- src/infra/db/mongodb/AccountMongoRepository.ts
+++ src/infra/db/mongodb/AccountMongoRepository.ts
@@ -1,10 +1,11 @@
 import { AddAccountRepository } from '../../../data/protocols/db/AddAccountRepository'
+import { LoadAccountByEmailRepository } from '../../../data/protocols/db/LoadAccountByEmailRepository'
 import { AccountModel } from '../../../domain/models/AccountModel'
 import { AddAccountModel } from '../../../domain/usecases/IAddAccountUseCase'
 import { MongoHelper } from './helper/MongoHelper'
 import { AccountMapper } from './mapper/AccountMapper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
@@ -13,5 +14,15 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountById = await accountCollection.findOne({ _id: result.insertedId })
 
     return AccountMapper.map(accountById)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+
+    const account = await accountCollection.findOne({ email })
+
+    if (!account) return null
+
+    return AccountMapper.map(account)
   }
 }
