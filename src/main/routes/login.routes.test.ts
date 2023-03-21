8ff9232d926457a2f74ee9c511955047cd90e85a
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-21 20:26:38 -0300

    refactor: use await before bcrypt adapter instance

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -38,7 +38,7 @@ describe('Login Routes', () => {
 
   describe('POST /login', () => {
     test('should return 200 on login', async () => {
-      const password = new BcryptAdapter(12).hash('123')
+      const password = await new BcryptAdapter(12).hash('123')
 
       await accountCollection.insertOne({
         name: 'Victor',
@@ -50,10 +50,19 @@ describe('Login Routes', () => {
         .post('/api/login')
         .send({
           email: 'victorteste@gmail.com',
-          password: '123',
-          passwordConfirmation: '123'
+          password: '123'
         })
         .expect(200)
+    })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'victorteste@gmail.com',
+          password: '123'
+        })
+        .expect(401)
     })
   })
 })
