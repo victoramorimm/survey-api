371b397a5fa338450139daa48262715ea0ee4c15
Victor Amorim <victorvmrgamer@gmail.com>
2023-03-21 07:32:10 -0300

    refactor: move EmailValidatorAdapter to main layer

--- src/main/adapters/validators/EmailValidatorAdapter.spec.ts
+++ src/main/adapters/validators/EmailValidatorAdapter.spec.ts
@@ -1,6 +1,6 @@
 import { EmailValidatorAdapter } from './EmailValidatorAdapter'
 import validator from 'validator'
-import { EmailValidator } from '../presentation/protocols'
+import { EmailValidator } from '../../../presentation/protocols'
 
 jest.mock('validator', () => ({
   isEmail (): boolean {
--- src/main/routes/signup.routes.ts
+++ src/main/routes/signup.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/expressRouteAdapter'
+import { adaptRoute } from '../adapters/express/expressRouteAdapter'
 import { makeSignUpController } from '../factories/signup/signup'
 
 export default (router: Router) => {
