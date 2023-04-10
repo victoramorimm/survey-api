723884ddcd6baa5179ec3654790a12f7c0a44820
Victor Amorim <victorvmrgamer@gmail.com>
2023-04-10 19:26:11 -0300

    feat: ensure usecase calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/addAccount/DbAddAccountUseCase.ts
+++ src/data/usecases/addAccount/DbAddAccountUseCase.ts
@@ -1,12 +1,16 @@
+import { LoadAccountByEmailRepository } from '../authentication/DbAuthenticationProtocols'
 import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher } from './DbAddAccountProtocols'
 
 export class DbAddAccountUseCase implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(account.email)
+
     const hashedPassword = await this.hasher.hash(account.password)
 
     const accountModel = await this.addAccountRepository.add({
